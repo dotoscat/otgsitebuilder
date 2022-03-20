0b8ecde805a2b3da7889acff7816bb28e00d21bf
dotoscat <dotoscat@gmail.com>
2022-03-20 13:05:21 +0100

    Force not null at option table

--- src/manager/database-struct.sql
+++ src/manager/database-struct.sql
@@ -29,10 +29,13 @@ CREATE TABLE IF NOT EXISTS Post (
 );
 
 CREATE TABLE IF NOT EXISTS Option (
-    title           VARCHAR (255)   DEFAULT "My Site",
+    title           VARCHAR (255)   NOT NULL
+                                    DEFAULT "My Site",
     posts_per_page  INTEGER         DEFAULT 3,
-    output          VARCHAR (255)   DEFAULT "output",
-    license         VARCHAR (1024)  DEFAULT ""
+    output          VARCHAR (255)   NOT NULL
+                                    DEFAULT "output",
+    license         VARCHAR (1024)  NOT NULL
+                                    DEFAULT ""
 );
 
 CREATE TABLE IF NOT EXISTS Category (
