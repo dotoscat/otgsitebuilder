d8c2fe6802ce23c63553e405d0555b90080cd724
Oscar Triano García <otrianodev@gmail.com>
2022-06-19 13:03:29 +0200

    Change database struct

--- src/manager/database-struct.sql
+++ src/manager/database-struct.sql
@@ -14,18 +14,20 @@
 
 CREATE TABLE IF NOT EXISTS Page (
     id        INTEGER       PRIMARY KEY AUTOINCREMENT,
-    name      VARCHAR (255) UNIQUE
+    title      VARCHAR (255) UNIQUE
                             NOT NULL,
-    reference VARCHAR (255) DEFAULT ""
-                            NOT NULL
+    content TEXT       DEFAULT ""
+                        NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS Post (
     id   INTEGER       PRIMARY KEY AUTOINCREMENT,
-    name VARCHAR (255) UNIQUE
+    title VARCHAR (255) UNIQUE
                        NOT NULL,
     date DATE          NOT NULL
-                       DEFAULT (CURRENT_DATE)
+                       DEFAULT (CURRENT_DATE),
+    content TEXT       DEFAULT ""
+                        NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS Option (
